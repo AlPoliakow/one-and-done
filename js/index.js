2d7051ecafd663498aa408f6d524bfdfb5252793
Al_P <al.e.poliakow@gmail.com>
2024-05-14 15:00:08 +1000

    Updated darkmode function to include droplist

--- js/index.js
+++ js/index.js
@@ -1,8 +1,18 @@
 const menu = document.querySelector('.toggle');
 const nav = document.querySelector('.full-screen-nav');
+const navClose = document.querySelector('.nav-close');
+const navHome = document.querySelector('.nav-home');
+const navHtml = document.querySelector('.nav-html');
+const navCss = document.querySelector('.nav-css');
+const navJs = document.querySelector('.nav-js');
+const navCombo = document.querySelector('.nav-combo');
+const navGit = document.querySelector('.nav-git');
+const navContact = document.querySelector('.nav-contact');
 const backdrop = document.querySelector('.backdrop');
 const body = document.querySelector('body');
 const darkModeSwitch = document.querySelector('input');
+const bars = document.querySelector('.toggle i');
+
 
 // nav menu
 menu.addEventListener('click', () => nav.classList.add('open-nav'));
@@ -17,10 +27,31 @@ function closeNav() {
 
 darkModeSwitch.addEventListener("click", function () {
     if (body.classList.contains("dark-mode")) {
-     body.classList.remove("dark-mode");
-     console.log("to light mode");
+        body.classList.remove("dark-mode");
+        bars.style.color = "black";
+        navClose.style.color = "black";
+        navHome.style.color = "black";
+        navHtml.style.color = "black";
+        navCss.style.color = "black";
+        navJs.style.color = "black";
+        navCombo.style.color = "black";
+        navGit.style.color = "black";
+        navContact.style.color = "black";
+        nav.classList.remove("dark-mode");
+        console.log("to light mode");
     } else {
-      body.classList.add("dark-mode");
-      console.log("to dark mode");
+        body.classList.add("dark-mode");
+        nav.classList.add("dark-mode");
+        bars.style.color = "white";
+        navClose.style.color = "white";
+        navHome.style.color = "white";
+        navHtml.style.color = "white";
+        navCss.style.color = "white";
+        navJs.style.color = "white";
+        navCombo.style.color = "white";
+        navGit.style.color = "white";
+        navContact.style.color = "white";
+        nav.classList.add("dark-mode");
+        console.log("to dark mode");
     }
-  });
+});
