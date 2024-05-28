b395208c761de531a46de380fde3df7cd7d47a1e
Al_P <al.e.poliakow@gmail.com>
2024-05-28 12:19:15 +1000

    Changed JS variables to prevent repeat error

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,11 @@
 const menu = document.querySelector('.toggle');
-const nav = document.querySelector('.full-screen-nav');
-const backdrop = document.querySelector('.backdrop');
+const navFs = document.querySelector('.full-screen-nav');
+const backdropNav = document.querySelector('.backdrop');
 
 // nav menu
-menu.addEventListener('click', () => nav.classList.add('open-nav'), nav.classList.remove("hide"));
-backdrop.addEventListener('click', () => nav.classList.remove('open-nav'));
+menu.addEventListener('click', () => navFs.classList.add('open-nav'), navFs.classList.remove("hide"));
+backdropNav.addEventListener('click', () => navFs.classList.remove('open-nav'));
 
 function closeNav() {
-    nav.classList.remove('open-nav');
+    navFs.classList.remove('open-nav');
 }
