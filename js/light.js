24386d44e37515e030afa52335da9ecd63b8dfdc
Al_P <al.e.poliakow@gmail.com>
2024-06-10 20:54:20 +1000

    Updated homepage light and dark styles

--- js/light.js
+++ js/light.js
@@ -10,16 +10,18 @@ const acknowledgement = document.querySelector('.acknowledgement');
 const socials = document.querySelectorAll('#socials i');
 const paragraphText = document.querySelectorAll('article p');
 const headings = document.querySelectorAll('article h2');
+const icon = document.querySelector('.logo');
 
 //light mode
 darkModeSwitch.addEventListener("click", function () {
     if (body.classList.contains("light-mode")) {
         body.classList.remove("light-mode");
+        icon.style.color="white";
         bars.style.color = "white";
         nav.style.background="black"; 
         acknowledgement.style.border="2px dotted white";
         darkModeSwitch.style.color="#ffef5c";
-        darkModeBox.style.border=-"dotted 1.5px #ffef5c";
+        //darkModeBox.style.border=-"dotted 1.5px #ffef5c";
         darkModeText.style.color="#ffef5c";
         darkModeText.innerText="Turn on the light?";
         //forEach loop for all multiples
@@ -39,10 +41,11 @@ darkModeSwitch.addEventListener("click", function () {
         body.classList.add("light-mode");
         nav.style.background="white"; //nav was still appearing with black background despite adding light-mode class
         bars.style.color = "black";
+        icon.style.color="black";
         nav.classList.remove("light-mode");
         acknowledgement.style.border="2px dotted black";
         darkModeSwitch.style.color="#3e390a";
-        darkModeBox.style.border="dotted 1.5px #3e390a";
+        //darkModeBox.style.border="dotted 1.5px #3e390a";
         darkModeText.style.color="#3e390a";
         darkModeText.innerText="Turn the light off!";
         //forEach loops for all multiples
