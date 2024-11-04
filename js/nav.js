9f85016705253f5be76eb5e49d56fb386d061790
Al_P <al.e.poliakow@gmail.com>
2024-11-04 17:15:40 +1100

    Removed Light mode JavaScript and added touch event for nav menu

--- js/nav.js
+++ js/nav.js
@@ -2,9 +2,13 @@ const menu = document.querySelector('.toggle');
 const navFs = document.querySelector('.full-screen-nav');
 const backdropNav = document.querySelector('.backdrop');
 
-// nav menu
+// nav menu for desktop
 menu.addEventListener('click', () => navFs.classList.add('open-nav'), navFs.classList.remove("hide"));
 backdropNav.addEventListener('click', () => navFs.classList.remove('open-nav'));
+//nav menu for mobile/tablet
+menu.addEventListener('touchstart', () => navFs.classList.add('open-nav'), navFs.classList.remove("hide"));
+backdropNav.addEventListener('touchstart', () => navFs.classList.remove('open-nav'));
+
 
 function closeNav() {
     navFs.classList.remove('open-nav');
